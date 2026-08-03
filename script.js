1fcb283b2274515941cbe70e1efda80207fa8baf
Zepmanbc <zepman@gmail.com>
2026-08-03 18:19:13 +0200

    setup exercices types and options

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 import { PIANO_SAMPLES } from "./const.js";
 
 const INCREMENT = 0.5;
+const INTERVALS_OPTIONS = ["Ascendant", "Descandant"];
+const CHORDS_OPTIONS = ["3 sons", "4 sons"];
+const MODES_OPTIONS = ["Ascendant", "Descandant"];
 
-const piano = new Tone.Sampler({
-  urls: PIANO_SAMPLES,
-  release: 1,
-  baseUrl: "./sounds/",
-}).toDestination();
-
-function playNotes(notes) {
-  Tone.loaded().then(() => {
-    const now = Tone.now();
-    let delta = 0;
-    for (const note of notes) {
-      piano.triggerAttackRelease(note, "4n", now + delta);
-      delta += INCREMENT;
-    }
-  });
+// Logic
+function getExerciseConfig() {
+  const exerciseType = document.querySelector(".type-btn.active").dataset.type;
+  const exerciseOptions = [
+    ...document.querySelectorAll(".option-btn.active"),
+  ].map((element) => element.dataset.value);
+  console.log("exerciseType : " + exerciseType);
+  console.log("exerciseOptions : " + exerciseOptions.join(", "));
+  return { exerciseType, exerciseOptions };
 }
 
 function playChord(notes) {
@@ -25,19 +22,94 @@ function playChord(notes) {
   });
 }
 
-document.getElementById("note").onclick = async () => {
-  Tone.loaded().then(() => {
-    playNotes(["C4"]);
-  });
-};
+// Interface
+const INTERVALS_KEYS = Object.keys(INTERVALS);
+const exerciseOptions = document.getElementById("exercise-options");
+const startStopBtn = document.getElementById("start-stop");
+const startinNote = document.getElementById("starting-note");
 
-document.getElementById("accord").onclick = async () => {
-  playChord(["C4", "E4", "G4"]);
-};
+let isPlaying = false;
 
-document.getElementById("arpege").onclick = async () => {
-  Tone.loaded().then(() => {
-    const now = Tone.now();
-    playNotes(["C4", "E4", "G4"]);
+function renderOptions(type) {
+  exerciseOptions.innerHTML = "";
+
+  let list = [];
+  if (type === "interval") list = INTERVALS_OPTIONS;
+  if (type === "chords") list = CHORDS_OPTIONS;
+  if (type === "mode") list = MODES_OPTIONS;
+
+  list.forEach((item) => {
+    const btn = document.createElement("button");
+    btn.classList.add("option-btn");
+    btn.dataset.value = item;
+    btn.textContent = item;
+    exerciseOptions.appendChild(btn);
+
+    btn.addEventListener("click", () => {
+      btn.classList.toggle("active");
+    });
   });
-};
+
+  // default active first
+  exerciseOptions.querySelector("button")?.classList.add("active");
+}
+
+function startSound() {
+  isPlaying = true;
+  startStopBtn.textContent = "🎵 Lecture";
+  startStopBtn.style.background = "#696969";
+
+  // runLoop();
+}
+
+function stopSound() {
+  isPlaying = false;
+  startStopBtn.textContent = "▶ Démarrer";
+  startStopBtn.style.background = "#2196f3";
+
+  // clearInterval(loopId);
+  // loopId = null;
+}
+
+function setupSwitch(groupId) {
+  const group = document.getElementById(groupId);
+  const buttons = group.querySelectorAll("button");
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      buttons.forEach((b) => b.classList.remove("active"));
+      btn.classList.add("active");
+    });
+  });
+}
+
+function setupToggle(groupId) {
+  const group = document.getElementById(groupId);
+  const buttons = group.querySelectorAll("button");
+
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      buttons.forEach((b) => b.classList.remove("active"));
+      btn.classList.add("active");
+    });
+  });
+}
+
+document.querySelectorAll(".type-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    document
+      .querySelectorAll(".type-btn")
+      .forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    renderOptions(btn.dataset.type);
+  });
+});
+
+// initializaton
+startStopBtn.addEventListener("click", () => {
+  startSound();
+});
+
+setupSwitch("exercise-type");
+renderOptions("interval");
